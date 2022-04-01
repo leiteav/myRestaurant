11027abb7738328c8be778a3f3c791d47a872cfd
Ariane Leite <arileitte@gmail.com>
2022-04-01 17:05:24 -0300

    style: implementando visual componente MenuCardapio

--- src/views/FolderPage.vue
+++ src/views/FolderPage.vue
@@ -1,85 +1,90 @@
 <template>
-  <ion-page>
+<ion-page>
     <ion-header :translucent="true">
-      <ion-toolbar>
-        <ion-buttons slot="start">
-          <ion-menu-button color="primary"></ion-menu-button>
-        </ion-buttons>
-        <ion-title>{{ $route.params.id }}</ion-title>
-      </ion-toolbar>
-    </ion-header>
-    
-    <ion-content :fullscreen="true">
-      <ion-header collapse="condense">
         <ion-toolbar>
-          <ion-title size="large">{{ $route.params.id }}</ion-title>
+            <ion-buttons slot="start">
+                <ion-menu-button color="primary"></ion-menu-button>
+            </ion-buttons>
+            <ion-title>{{ $route.params.id }}</ion-title>
         </ion-toolbar>
-      </ion-header>
-    
-      <div id="container">
-        <div v-if="checarCard($route.params.id) == 1"> 
-          <MenuCardapio/>
+    </ion-header>
+
+    <ion-content :fullscreen="true">
+        <ion-header collapse="condense">
+            <ion-toolbar>
+                <ion-title size="large">{{ $route.params.id }}</ion-title>
+            </ion-toolbar>
+        </ion-header>
+
+        <div id="container">
+            <div v-if="checarCard($route.params.id) == 1">
+                <MenuCardapio />
+            </div>
         </div>
-      </div>
     </ion-content>
-  </ion-page>
+</ion-page>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
-import { IonButtons, IonContent, IonHeader, IonMenuButton, IonPage, IonTitle, IonToolbar } from '@ionic/vue';
-import MenuCardapio from './MenuCardapio'
-export default defineComponent({
-  name: 'FolderPage',
-  components: {
+import { 
     IonButtons,
     IonContent,
     IonHeader,
     IonMenuButton,
     IonPage,
     IonTitle,
-    IonToolbar,
-    MenuCardapio
-  }, 
-  afterMount(){
-    console.log(this.$route)
-  },
-  methods: {
-  checarCard(value){
-      switch (value) {
-        case "Menu":
-          return 1;
-        default:
-          return 0;  
-      }
+    IonToolbar
+} from '@ionic/vue';
+import MenuCardapio from './MenuCardapio.vue'
+export default defineComponent({
+    name: 'FolderPage',
+    components: {
+        IonButtons,
+        IonContent,
+        IonHeader,
+        IonMenuButton,
+        IonPage,
+        IonTitle,
+        IonToolbar,
+        MenuCardapio
+    },
+    methods: {
+        checarCard(id) {
+            switch (id) {
+                case "Menu":
+                    return 1;
+                default:
+                    return 0;
+            }
+        }
     }
-  }
 });
 </script>
 
 <style scoped>
 #container {
-  text-align: center;
-  position: absolute;
-  left: 0;
-  right: 0;
-  top: 50%;
-  transform: translateY(-50%);
+    text-align: center;
+    position: absolute;
+    left: 0;
+    right: 0;
+    top: 50%;
+    transform: translateY(-50%);
 }
 
 #container strong {
-  font-size: 20px;
-  line-height: 26px;
+    font-size: 20px;
+    line-height: 26px;
 }
 
 #container p {
-  font-size: 16px;
-  line-height: 22px;
-  color: #8c8c8c;
-  margin: 0;
+    font-size: 16px;
+    line-height: 22px;
+    color: #8c8c8c;
+    margin: 0;
 }
 
 #container a {
-  text-decoration: none;
+    text-decoration: none;
 }
 </style>
--- src/views/MenuCardapio.vue
+++ src/views/MenuCardapio.vue
@@ -1,25 +1,151 @@
 <template>
-<p>
-{{ test }}
-</p>
+<div>
+<div class="body-layout">
+    <h1> Pizzas </h1>
+        <div v-for="(slide, index) in slides" lines="none" :key="index">
+            <img :src="baseAssetsImages + slide">
+        </div>
+
+    <ion-list>
+          <ion-list-header> Salgadas</ion-list-header>
+
+          <ion-item>
+            <ion-avatar slot="start">
+             <img :src="baseAssetsImages + imgSabores[2]">
+            </ion-avatar>
+            <ion-label>
+              <h2>Mussarela</h2>
+              <p>Tomates, queijo mussarela e majericão.</p>
+            </ion-label>
+          </ion-item>
+
+          <ion-item>
+            <ion-avatar slot="start">
+             <img :src="baseAssetsImages + imgSabores[1]">
+            </ion-avatar>
+            <ion-label>
+              <h2>Napolitana</h2>
+              <p>Parmesão, queijo, tomates, orégano e alho.</p>
+            </ion-label>
+          </ion-item>
+
+         <ion-item>
+            <ion-avatar slot="start">
+             <img :src="baseAssetsImages + imgSabores[0]">
+            </ion-avatar>
+            <ion-label>
+              <h2>Portuguesa</h2>
+              <p>Mussarela, tiras de presunto, cebola e azeitonas verdes e cream cheese.</p>
+            </ion-label>
+          </ion-item>
+
+          <ion-item>
+            <ion-avatar slot="start">
+             <img :src="baseAssetsImages + imgSabores[1]">
+            </ion-avatar>
+            <ion-label>
+              <h2>Frango</h2>
+              <p>Frango desfiado e tomate.</p>
+            </ion-label>
+          </ion-item>         
+    </ion-list>
+
+      <ion-list>
+          <ion-list-header> Doces</ion-list-header>
+
+          <ion-item>
+            <ion-avatar slot="start">
+             <img :src="baseAssetsImages + imgSabores[2]">
+            </ion-avatar>
+            <ion-label>
+              <h2>Brigadeiro</h2>
+              <p>Creme de chocolate.</p>
+            </ion-label>
+          </ion-item>
+
+          <ion-item>
+            <ion-avatar slot="start">
+             <img :src="baseAssetsImages + imgSabores[1]">
+            </ion-avatar>
+            <ion-label>
+              <h2>Romeu e Julieta</h2>
+              <p>Queijo branco com goiabada.</p>
+            </ion-label>
+          </ion-item>
+
+         <ion-item>
+            <ion-avatar slot="start">
+             <img :src="baseAssetsImages + imgSabores[0]">
+            </ion-avatar>
+            <ion-label>
+              <h2>Banana com Doce de Leite</h2>
+              <p>Banana com creme de doce de leite.</p>
+            </ion-label>
+          </ion-item>
+
+          <ion-item>
+            <ion-avatar slot="start">
+             <img :src="baseAssetsImages + imgSabores[1]">
+            </ion-avatar>
+            <ion-label>
+              <h2>Marshmellow</h2>
+              <p>Marshmellow com caramelo salgado.</p>
+            </ion-label>
+          </ion-item>         
+    </ion-list>
+</div>
+
+    <ion-fab vertical="bottom" horizontal="end" slot="fixed" @click="finalizarPedido">
+        <ion-fab-button>
+        <ion-icon :icon="arrowForwardCircle"></ion-icon>
+        </ion-fab-button>
+    </ion-fab>
+
+    </div>
 </template>
+
 <script lang="ts">
 import { defineComponent } from 'vue';
-export default defineComponent({
-name: "MenuCardapio",
-data() {
-    return {
-        test: "teste"
-    }
-},
-methods:{
+import { arrowForwardCircle } from "ionicons/icons";
 
-}
+export default defineComponent({
+    name: "MenuCardapio",
+    components : {
+    },
+    data() {
+        return {
+            baseAssetsImages: "./assets/images/",
+            slides: ["slide-pizza-1.jpg"],
+            imgSabores : ["slide-pizza-1.jpg","slide-pizza-2.jpg","slide-pizza-3.jpg"],
+            arrowForwardCircle: arrowForwardCircle
+        }
+    },
+    methods: {
+    async finalizarPedido(){
+        alert("Deseja finalizar o pedido?");
+    }
+    }
 
 });
 </script>
+
 <style scoped>
-p {
+.body-layout {
+    height: 100%;
+    margin: 20px;
     color: white;
+    align-items: start;
+
+}
+h1{
+    text-align: left;
+}
+
+img {
+    width: 370px;
+    border-radius: 15px;
+}
+ion-footer{
+    position: fixed;
 }
 </style>
